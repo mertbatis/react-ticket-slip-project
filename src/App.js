6fb1e299fb02cf054be6c848b71c21936be14ed6
unknown <brusmarvy@gmail.com>
2023-09-04 00:28:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import DistanceSystem from './DistanceSystem';
 import { GoogleMap,useLoadScript,Marker} from '@react-google-maps/api';
-
 function App() {
- // .env dosyasındaki değişkeni al
-const googleMapsApiKey = process.env.REACT_APP_API;
-document.getElementById('google-maps-script').src =`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places`;
-
   return (
     <div className='container'>
       <DistanceSystem />
     </div>
   );
 }
-
 export default App;
